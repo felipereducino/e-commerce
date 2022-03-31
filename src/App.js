5362f61559bda654e6cebb55894cf4c90c82e8e9
felipereducino <felipe-reducino@hotmail.com>
2022-03-31 17:22:34 -0300

    first push

---
+++ src/App.js
@@ -0,0 +1,44 @@
+
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-cotainer">
+        {/* <img /> */}
+        <div className="category-body-cotainer">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-cotainer">
+        {/* <img /> */}
+        <div className="category-body-cotainer">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-cotainer">
+        {/* <img /> */}
+        <div className="category-body-cotainer">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-cotainer">
+        {/* <img /> */}
+        <div className="category-body-cotainer">
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-cotainer">
+        {/* <img /> */}
+        <div className="category-body-cotainer">
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
